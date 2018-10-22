b954c83a0688146097efba5f2ac670446e4b2ec5
thlepsch <ymerza@gmail.com>
2018-10-22 16:10:51 -0400

    should be fine now

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# just a change because sticky keys
 ##Ask an IP address
 user_input = input('Please enter an IPv4 IP address:')
 print("This is your inputted IPv4 address:" + user_input)
